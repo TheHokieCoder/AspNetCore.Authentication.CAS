aa50058739790cfba3738bda323e94fb63abefac
TheHokieCoder <13714570+TheHokieCoder@users.noreply.github.com>
2020-07-21 14:47:05 -0400

    Fully documented the CasOptions object

--- src/CasOptions.cs
+++ src/CasOptions.cs
@@ -4,6 +4,9 @@
 	using Microsoft.AspNetCore.Http;
 
 
+	/// <summary>
+	///		An implementation of <see cref="RemoteAuthenticationOptions"/> specifically for remote authentication with a CAS server.
+	/// </summary>
 	public class CasOptions : RemoteAuthenticationOptions
 	{
 		#region Constants/Enums
@@ -19,11 +22,6 @@
 		public string AttributesParentNodeName { get; set; } = "attributes";
 
 		/// <summary>
-		///		The caption text that can be displayed on a sign-in user interface.
-		/// </summary>
-		public string Caption { get; set; }
-
-		/// <summary>
 		///		The base URL of the CAS server.
 		/// </summary>
 		public string CasServerUrlBase { get; set; }
@@ -37,13 +35,12 @@
 		public string CasValidationUrl { get; set; }
 
 		/// <summary>
-		///		TODO: document this property
+		///		An object that provides access to events that occur during the CAS authentication process.
 		/// </summary>
-		public bool EscapeServiceString { get; set; } = true;
-
-		/// <summary>
-		///		TODO: document this property
-		/// </summary>
+		/// <remarks>
+		///		By default, this property will reference the base <see cref="RemoteAuthenticationEvents"/> property, but it can be overridden by an
+		///		instance of <see cref="CasEvents"/>.
+		/// </remarks>
 		public new CasEvents Events
 		{
 			get => (CasEvents)base.Events;
@@ -51,8 +48,13 @@
 		}
 
 		/// <summary>
-		///		TODO: document this property
+		///		Indicates whether or not the client should indicate to the CAS server that it should use gateway mode, which means that authentication
+		///		is not critical to access the resource.
 		/// </summary>
+		/// <remarks>
+		///		See https://apereo.github.io/cas/6.2.x/protocol/CAS-Protocol-Specification.html#211-parameters for more information about the
+		///		"gateway" parameter.
+		/// </remarks>
 		public bool Gateway { get; set; }
 
 		/// <summary>
@@ -61,22 +63,26 @@
 		public string NameIdentifierAttribute { get; set; }
 
 		/// <summary>
-		///		Indicates whether or not clients should always be forced to authenticate (true) or if an existing, valid service ticket will suffice
-		///		for authentication (false).
+		///		Indicates whether or not clients should always be forced to present credientials (true) or if an existing, valid service ticket will
+		///		suffice for authentication (false).
 		/// </summary>
 		/// <remarks>
 		///		Setting this option to "true" effectively disables single sign-on (SSO) for the application as the client will always be required to
-		///		present credentials.
+		///		present credentials. See https://apereo.github.io/cas/6.2.x/protocol/CAS-Protocol-Specification.html#211-parameters for more
+		///		information about the "renew" parameter.
 		/// </remarks>
 		public bool Renew { get; set; }
 
 		/// <summary>
-		///		TODO: document this property
+		///		Force the CAS client to always present the service (client application) URL using the HTTPS scheme. 
 		/// </summary>
+		/// <remarks>
+		///		This property is useful in scenarios where CAS servers are configured to refuse insecure (HTTP) connections to services.
+		/// </remarks>
 		public bool ServiceForceHttps { get; set; } = false;
 
 		/// <summary>
-		///		TODO: document this property
+		///		The host name to be presented to the CAS server via the "service" parameter.
 		/// </summary>
 		public string ServiceHost { get; set; }
 
@@ -86,7 +92,7 @@
 		public string ServiceResponseNamespace { get; set; }
 
 		/// <summary>
-		///		TODO: document this property
+		///		The format with which details about the authentication session should be stored.
 		/// </summary>
 		public ISecureDataFormat<AuthenticationProperties> StateDataFormat { get; set; }
 
@@ -97,6 +103,9 @@
 		#endregion Fields/Properties
 
 		#region Methods
+		/// <summary>
+		///		Default constructor.
+		/// </summary>
 		public CasOptions()
 		{
 			// Configure default CAS options
