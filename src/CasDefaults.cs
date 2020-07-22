1acc26ba7de63eb1e6b960a9e24b47ea21e211e5
TheHokieCoder <13714570+TheHokieCoder@users.noreply.github.com>
2020-07-22 11:06:13 -0400

    Added static defaults class to provide built-in defaults

---
+++ src/CasDefaults.cs
@@ -0,0 +1,24 @@
+﻿namespace AspNetCore.Authentication.CAS
+{
+	/// <summary>
+	///		A collection of constants and default values used by the authentication middleware.
+	/// </summary>
+	public static class CasDefaults
+	{
+		/// <summary>
+		///		A collection of constants and default values relating to the authentication scheme used by the authentication middleware.
+		/// </summary>
+		public static class AuthenticationScheme
+		{
+			/// <summary>
+			///		The display name of the authentication scheme used by the authentication middleware.
+			/// </summary>
+			public const string DISPLAY_NAME = "CAS";
+
+			/// <summary>
+			///		The identifier of the authentication scheme used by the authentication middleware.
+			/// </summary>
+			public const string SCHEME = "CAS";
+		}
+	}
+}
--- src/CasExtensions.cs
+++ src/CasExtensions.cs
@@ -13,19 +13,43 @@
 	public static class CasExtensions
 	{
 		#region Constants/Enums
-		private const string AUTHENTICATION_SCHEME_DEFAULT = "CAS";
-
-		private const string DISPLAY_NAME_DEFAULT = "CAS";
 		#endregion Constants/Enums
 
 		#region Fields/Properties
 		#endregion Fields/Properties
 
 		#region Methods
-		public static AuthenticationBuilder AddCas(this AuthenticationBuilder builder, Action<CasOptions> configureOptions)
-		{
-			return builder.AddCas(AUTHENTICATION_SCHEME_DEFAULT, DISPLAY_NAME_DEFAULT, configureOptions);
-		}
+		/// <summary>
+		///		Adds the CAS remote authentication scheme to the authentication middleware.
+		/// </summary>
+		///	<returns>
+		///		The authentication builder that has the CAS remote authentication scheme configured for use
+		/// </returns>
+		/// <remarks>
+		///		This overload of the extension method uses the default values provided in <see cref="CasDefaults.AuthenticationScheme"/> to specify
+		///		the authentication scheme and display name, and provides an empty object for the configuration options.
+		/// </remarks>
+		public static AuthenticationBuilder AddCas(this AuthenticationBuilder builder) =>
+			builder.AddCas(CasDefaults.AuthenticationScheme.SCHEME, CasDefaults.AuthenticationScheme.DISPLAY_NAME, _ => { });
+
+		/// <summary>
+		///		Adds the CAS remote authentication scheme to the authentication middleware.
+		/// </summary>
+		/// <param name="builder">
+		///		The authentication builder
+		/// </param>
+		/// <param name="configureOptions">
+		///		An action method that specifies CAS options
+		/// </param>
+		/// <returns>
+		///		The authentication builder that has the CAS remote authentication scheme configured for use
+		/// </returns>
+		/// <remarks>
+		///		This overload of the extension method uses the default values provided in <see cref="CasDefaults.AuthenticationScheme"/> to specify
+		///		the authentication scheme and display name.
+		/// </remarks>
+		public static AuthenticationBuilder AddCas(this AuthenticationBuilder builder, Action<CasOptions> configureOptions) =>
+			builder.AddCas(CasDefaults.AuthenticationScheme.SCHEME, CasDefaults.AuthenticationScheme.DISPLAY_NAME, configureOptions);
 
 		/// <summary>
 		///		Adds the CAS remote authentication scheme to the authentication middleware.
@@ -45,7 +69,8 @@
 		/// <returns>
 		///		The authentication builder that has the CAS remote authentication scheme configured for use
 		/// </returns>
-		public static AuthenticationBuilder AddCas(this AuthenticationBuilder builder, string authenticationScheme, string displayName, Action<CasOptions> configureOptions)
+		public static AuthenticationBuilder AddCas(this AuthenticationBuilder builder, string authenticationScheme, string displayName,
+			Action<CasOptions> configureOptions)
 		{
 			builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IPostConfigureOptions<CasOptions>, CasPostConfigureOptions>());
 
